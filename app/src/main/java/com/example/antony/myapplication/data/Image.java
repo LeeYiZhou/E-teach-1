10f7bf0122d2be41f20aca8c0997aad66408f95a
YiZhou Li <401788098@qq.com>
2018-02-24 13:23:05 +0800

    李一洲_UI_2018_2_24

--- app/src/main/java/com/example/antony/myapplication/data/Image.java
+++ app/src/main/java/com/example/antony/myapplication/data/Image.java
@@ -5,4 +5,17 @@ package com.example.antony.myapplication.data;
  */
 
 public class Image {
+    private int imageId;
+
+    public Image(int imageId) {
+        this.imageId = imageId;
+    }
+
+    public int getImageId() {
+        return imageId;
+    }
+
+    public void setImageId(int imageId) {
+        this.imageId = imageId;
+    }
 }
